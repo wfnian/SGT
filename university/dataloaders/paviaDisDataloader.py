455a940986b25d1fc2cee9db09289de82a318991
wfnian <wfnian@gmail.com>
2023-03-26 10:12:20 +0800

    Update paviaDisDataloader.py

--- university/dataloaders/paviaDisDataloader.py
+++ university/dataloaders/paviaDisDataloader.py
@@ -8,11 +8,11 @@ class Dataset_india(Dataset):
     def __init__(self, flag='train') -> None:
         assert flag in ['train', 'val'], 'not implement!'
         self.flag = flag
-        feature = pd.read_csv("/home/wfnian/heart/高光谱_SAR数据_王方年/高光谱空间不相交数据/论文对比/Pavia/{}_feature.txt".format(
+        feature = pd.read_csv("/home/wfnian/heart/高光谱_SAR数据_/高光谱空间不相交数据/论文对比/Pavia/{}_feature.txt".format(
             self.flag),
                               header=None,
                               sep="	")
-        label = pd.read_csv("/home/wfnian/heart/高光谱_SAR数据_王方年/高光谱空间不相交数据/论文对比/Pavia/{}_label.txt".format(self.flag),
+        label = pd.read_csv("/home/wfnian/heart/高光谱_SAR数据_/高光谱空间不相交数据/论文对比/Pavia/{}_label.txt".format(self.flag),
                             header=None)
         self.feature = feature.values
         self.label = label.values[:, 0]
